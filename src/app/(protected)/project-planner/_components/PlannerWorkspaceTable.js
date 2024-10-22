a50f6d042d41425bafda6a8b08b9f8f14481170a
denniscloudmod <dennis@cloudmod.io>
2024-10-22 19:21:21 +0300

    Update PlannerWorkspaceTable.js

--- src/app/(protected)/project-planner/_components/PlannerWorkspaceTable.js
+++ src/app/(protected)/project-planner/_components/PlannerWorkspaceTable.js
@@ -172,7 +172,8 @@ const DropdownMenu = ({ onDelete }) => {
                     onDelete();
                     closeModal();
                   }}
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium sm:ml-3 sm:w-auto sm:text-sm"
+                  // className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 sm:ml-3 sm:w-auto sm:text-sm"
                 >
                   Delete
                 </button>
